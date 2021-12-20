731abf2bba9dc7fb56a47646ed2c83779046651a
ofadiman <ofadiman@gmail.com>
2021-12-20 16:41:34 +0100

    feat: create users controller scaffold

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common'
+import { Controller, Delete, Get, Patch, Post } from '@nestjs/common'
+import { UsersService } from './users.service'
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  public constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  public async createUser(): Promise<unknown> {
+    // TODO: Implement route.
+    return
+  }
+
+  @Get(`:userId`)
+  public async getUser(): Promise<unknown> {
+    // TODO: Implement route.
+    return
+  }
+
+  @Patch(`:userId`)
+  public async updateUser(): Promise<unknown> {
+    // TODO: Implement route.
+    return
+  }
+
+  @Delete(`:userId`)
+  public async deleteUser(): Promise<unknown> {
+    // TODO: Implement route.
+    return
+  }
+
+  @Get()
+  public async paginate(): Promise<unknown> {
+    // TODO: Implement route.
+    return
+  }
+}

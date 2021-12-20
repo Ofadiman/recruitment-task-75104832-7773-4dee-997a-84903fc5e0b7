80b40bd81de84af96eecb30b40297a9784506b17
ofadiman <ofadiman@gmail.com>
2021-12-20 19:52:14 +0100

    feat: create user by publishing message

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,20 @@ import { AppService } from './app.service'
 import { DatabaseModule } from './database/database.module'
 import { UsersModule } from './users/users.module'
 import { APP_PIPE } from '@nestjs/core'
+import { RedisModule } from '@liaoliaots/nestjs-redis'
 
 @Module({
-  imports: [DatabaseModule, UsersModule],
+  imports: [
+    DatabaseModule,
+    UsersModule,
+    RedisModule.forRoot({
+      closeClient: true,
+      config: {
+        host: 'recruitment-task-75104832-7773-4dee-997a-84903fc5e0b7-redis',
+        port: 6379
+      }
+    })
+  ],
   controllers: [AppController],
   providers: [
     AppService,
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,17 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common'
+import { Body, Controller, Delete, Get, OnModuleInit, Param, Patch, Post } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { CreateUserBodyDto, CreateUserResponseDto } from './dto/create-user.dto'
 import { GetUserByIdParamsDto, GetUserByIdResponseDto } from './dto/get-user-by-id.dto'
 import { UpdateUserBodyDto, UpdateUserParamsDto, UpdateUserResponseDto } from './dto/update-user.dto'
 import { DeleteUserParamsDto } from './dto/delete-user.dto'
+import { InjectRedis } from '@liaoliaots/nestjs-redis'
+import { Redis } from 'ioredis'
+
+const CREATE_USER_CHANNEL = `CREATE_USER_CHANNEL`
 
 @Controller('users')
-export class UsersController {
-  public constructor(private readonly usersService: UsersService) {}
+export class UsersController implements OnModuleInit {
+  public constructor(private readonly usersService: UsersService, @InjectRedis() private readonly redis: Redis) {}
 
   @Post()
   public async createUser(@Body() body: CreateUserBodyDto): Promise<CreateUserResponseDto> {
@@ -36,5 +40,33 @@ export class UsersController {
   public async paginate(): Promise<unknown> {
     // TODO: Implement route.
     return
+  }
+
+  @Post('publish-user')
+  public async publishUser(@Body() body: CreateUserBodyDto) {
+    const publisher = this.redis.duplicate()
+    await publisher.publish(CREATE_USER_CHANNEL, JSON.stringify(body))
+  }
+
+  async onModuleInit() {
+    const subscriber = this.redis.duplicate()
+    subscriber.subscribe(CREATE_USER_CHANNEL, (err, count) => {
+      if (err) {
+        console.error('Failed to subscribe: %s', err.message)
+      } else {
+        console.log(`Subscribed successfully! This client is currently subscribed to ${count} channels.`)
+      }
+    })
+
+    subscriber.on('message', async (channel, message) => {
+      console.info(`Received ${message} from ${channel}`)
+      if (channel === CREATE_USER_CHANNEL) {
+        try {
+          await this.usersService.createUser(JSON.parse(message))
+        } catch (error) {
+          console.error(error)
+        }
+      }
+    })
   }
 }

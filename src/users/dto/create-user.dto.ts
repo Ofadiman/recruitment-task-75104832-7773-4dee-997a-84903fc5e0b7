86107dc770cc3975092faceb9b81e9ea3ed75a4a
ofadiman <ofadiman@gmail.com>
2021-12-20 17:46:31 +0100

    refactor: move validation away from user entity

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,22 @@
-import { PickType } from '@nestjs/swagger'
-import { User } from '../user.entity'
+import { MAXIMUM_FIRST_NAME_LENGTH, MAXIMUM_LAST_NAME_LENGTH } from '../user.entity'
+import { IsEmail, IsString, MaxLength } from 'class-validator'
 
-export class CreateUserBodyDto extends PickType(User, [`firstName`, `email`, `lastName`]) {}
+export class CreateUserBodyDto {
+  @IsEmail()
+  public email: string
 
-export class CreateUserResponseDto extends PickType(User, [`firstName`, `email`, `lastName`, `id`]) {}
+  @IsString()
+  @MaxLength(MAXIMUM_FIRST_NAME_LENGTH)
+  public firstName: string
+
+  @IsString()
+  @MaxLength(MAXIMUM_LAST_NAME_LENGTH)
+  public lastName: string
+}
+
+export class CreateUserResponseDto {
+  public email: string
+  public firstName: string
+  public id: string
+  public lastName: string
+}
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,5 +1,4 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
-import { IsEmail, IsString, IsUUID, MaxLength } from 'class-validator'
 
 export const MAXIMUM_FIRST_NAME_LENGTH = 50
 export const MAXIMUM_LAST_NAME_LENGTH = 50
@@ -7,20 +6,14 @@ export const MAXIMUM_LAST_NAME_LENGTH = 50
 @Entity({ name: `users` })
 export class User {
   @PrimaryGeneratedColumn('uuid')
-  @IsUUID()
   public id: string
 
   @Column()
-  @IsEmail()
   public email: string
 
   @Column({ length: MAXIMUM_FIRST_NAME_LENGTH })
-  @IsString()
-  @MaxLength(MAXIMUM_FIRST_NAME_LENGTH)
   public firstName: string
 
   @Column({ length: MAXIMUM_LAST_NAME_LENGTH })
-  @IsString()
-  @MaxLength(MAXIMUM_LAST_NAME_LENGTH)
   public lastName: string
 }
